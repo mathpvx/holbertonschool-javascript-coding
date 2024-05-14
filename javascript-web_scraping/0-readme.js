df5a33512c85202cb6ce245bd2a15220834fc073
mathpvx <7547@holbertonstudents.com>
2024-05-14 11:56:57 +0200

    semistandard fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data.toString());
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
 });
