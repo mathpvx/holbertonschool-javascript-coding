1091c829b09fcc5b03da6eae110af1f4a811bf5d
mathpvx <7547@holbertonstudents.com>
2024-05-24 17:04:20 +0200

    update

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,7 +1,6 @@
-// built in module that provides functionality
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   try {
     // Read the file synchronously.
     const data = fs.readFileSync(path, 'utf8').trim();
@@ -31,6 +30,7 @@ function countStudents (path) {
         }
       }
     }
+
     // Log the number of students and their lists.
     console.log(`Number of students: ${csCount + sweCount}`);
     console.log(`Number of students in CS: ${csCount}. List: ${csList.join(', ')}`);
