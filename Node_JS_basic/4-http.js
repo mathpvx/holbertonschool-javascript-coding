bd19a6d7b4efb1c49d45619d06bd78ca65fc3da4
mathpvx <7547@holbertonstudents.com>
2024-05-28 14:29:08 +0200

    update

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,12 +3,12 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((request, response) => {
-	response.writeHead(200, { 'Content-Type': 'text/plain' });
-	response.end('Hello Holberton School!');
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-	console.log(`Server running at http://127.0.0.1:${port}/`);
+  console.log(`Server running at http://127.0.0.1:${port}/`);
 });
 
 module.exports = app;
